0b8fa79381ea7ae1d3a66aba403ae63bfb527e88
Eduardo AG <edag4000@gmail.com>
2022-11-24 11:46:43 -0600

    Agregando server a proyecto y script de cliente

--- Evidencia_2_Trafic_Simulation/Assets/Scripts/WebClient.cs
+++ Evidencia_2_Trafic_Simulation/Assets/Scripts/WebClient.cs
@@ -5,15 +5,41 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
 
+[Serializable]
+public struct CarInfo
+{
+    public int[] pos;
+    public string name;
+}
+[Serializable]
+public struct TrafficLights
+{
+    public int[] pos;
+    public string status;
+}
+[Serializable]
+struct Step
+{
+    public CarInfo[] cars;
+    public TrafficLights[] tf;
+}
+
 public class WebClient : MonoBehaviour
 {
     List<List<Vector3>> positions;
     public GameObject[] cars;
+    public GameObject[] tfs;
     public float timeToUpdate = 5.0f;
     private float timer;
     public float dt;
     int numPositions;
     int currPosition;
+    Step step;
+    bool firstStep = true;
+    bool received = false;
+    float chrono = 0.0f;
+    Vector3[] prevPositions;
+    Vector3[] moveVects;
 
     // IEnumerator - yield return
     IEnumerator SendData(string data)
@@ -21,17 +47,16 @@ public class WebClient : MonoBehaviour
         WWWForm form = new WWWForm();
         form.AddField("bundle", "the data");
         //string url = "http://localhost:8585";
-        string url = "http://localhost:8585/multiagentes";
+        string url = "http://localhost:8000/";
 
         //string url = "https://tec-server.mybluemix.net/api/points";
         //string url = "https://tec-server-demo.mybluemix.net/api/points";
         //using (UnityWebRequest www = UnityWebRequest.Post(url, form))
         using (UnityWebRequest www = UnityWebRequest.Get(url))
         {
-            byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(data);
+            byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes("");
             www.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
             www.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
-            //www.SetRequestHeader("Content-Type", "text/html");
             www.SetRequestHeader("Content-Type", "application/json");
 
             yield return www.SendWebRequest();          // Talk to Python
@@ -41,109 +66,88 @@ public class WebClient : MonoBehaviour
             }
             else
             {
-                //Debug.Log(www.downloadHandler.text);    // Answer from Python
-                //Debug.Log("Form upload complete!");
-                //Data tPos = JsonUtility.FromJson<Data>(www.downloadHandler.text.Replace('\'', '\"'));
-                //Debug.Log(tPos);
-                List<Vector3> newPositions = new List<Vector3>();
-                string txt = www.downloadHandler.text.Replace('\'', '\"');
-                txt = txt.TrimStart('"', '{', 'd', 'a', 't', 'a', ':', '[');
-                txt = "{\"" + txt;
-                txt = txt.TrimEnd(']', '}');
-                //txt = txt + '}';
-                string[] strs = txt.Split(new string[] { "}, {" }, StringSplitOptions.None);
-                Debug.Log("strs.Length:" + strs.Length);
-                for (int i = 0; i < strs.Length; i++)
+                step = JsonUtility.FromJson<Step>(www.downloadHandler.text);
+                Debug.Log("<-------RESPONSE------->");
+                if (!firstStep)
                 {
-                    strs[i] = strs[i].Trim();
-                    if (i == 0) strs[i] = strs[i] + '}';
-                    else if (i == strs.Length - 1) strs[i] = '{' + strs[i] + '}';
-                    else strs[i] = '{' + strs[i] + '}';
-                    Debug.Log(strs[i]);
-                    Vector3 test = JsonUtility.FromJson<Vector3>(strs[i]);
-                    newPositions.Add(test);
+                    moveVects = new Vector3[cars.Length];
+                    for (int i = 0; i < cars.Length; i++)
+                    {
+                        Vector3 goal = new Vector3(step.cars[i].pos[0], 0.5f, step.cars[i].pos[1]);
+                        Vector3 displacement = goal - cars[i].transform.position;
+                        if (displacement.magnitude > 3.0f)
+                        {
+                            displacement.Normalize();
+                            cars[i].transform.position = goal + displacement * 3;
+                            displacement = goal - cars[i].transform.position;
+                        }
+                        moveVects[i] = displacement;
+                    }
                 }
-
-                List<Vector3> poss = new List<Vector3>();
-                for (int s = 0; s < cars.Length; s++)
-                {
-                    //spheres[s].transform.localPosition = newPositions[s];
-                    poss.Add(newPositions[s]);
-                }
-                positions.Add(poss);
+                received = true;
             }
-            numPositions++;
         }
     }
 
     // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
-        numPositions = 0;
-        currPosition = 0;
-        positions = new List<List<Vector3>>();
-        Debug.Log(cars.Length);
-        for(int i = 0; i < cars.Length; i++)
-        {
-            float r = UnityEngine.Random.Range(0f, 1f);
-            float g = UnityEngine.Random.Range(0f, 1f);
-            float b = UnityEngine.Random.Range(0f, 1f);
-            cars[i].GetComponent<MeshRenderer>().material.SetColor("_Color", new Color(r, g, b));
-        }
-#if UNITY_EDITOR
-         Vector3 fakePos = new Vector3(3.44f, 0, -15.707f);
+        #if UNITY_EDITOR
+        Vector3 fakePos = new Vector3(3.44f, 0, -15.707f);
         string json = EditorJsonUtility.ToJson(fakePos);
         //StartCoroutine(SendData(call));
         StartCoroutine(SendData(json));
         timer = timeToUpdate;
-#endif
+        #endif
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-         *    5 -------- 100
-         *    timer ----  ?
-         */
-        timer -= Time.deltaTime;
-        dt = 1.0f - (timer / timeToUpdate);
-        if(numPositions - currPosition > 0)
+        if (firstStep)
         {
-            currPosition++;
-            dt = 0.0f;
-        }
-        else if(dt < 0.001f)
-        {
-            dt = 1.0f;
-        }
-
-        if (timer < 0)
-        {
-#if UNITY_EDITOR
-            timer = timeToUpdate; // reset the timer
-            Vector3 fakePos = new Vector3(3.44f, 0, -15.707f);
-            string json = EditorJsonUtility.ToJson(fakePos);
-            StartCoroutine(SendData(json));
-#endif
-        }
-
-
-        if (positions.Count > 1)
-        {
-            for (int s = 0; s < cars.Length; s++)
+            if (received)
             {
-                // Get the last position for s
-                List<Vector3> last = positions[positions.Count - 1];
-                // Get the previous to last position for s
-                List<Vector3> prevLast = positions[positions.Count - 2];
-                // Interpolate using dt
-                // A + t(B-A)
-                Vector3 interpolated = Vector3.Lerp(prevLast[s], last[s], dt);
-                cars[s].transform.localPosition = interpolated;
+                firstStep = false;
+                for (int i = 0; i < cars.Length; i++)
+                {
+                    Vector3 pos = new Vector3(step.cars[i].pos[0], 0.5f, step.cars[i].pos[1]);
+                    cars[i].transform.position = pos;
+                }
+                #if UNITY_EDITOR
+                timer = timeToUpdate; // reset the timer
+                Vector3 fakePos = new Vector3(3.44f, 0, -15.707f);
+                string json = EditorJsonUtility.ToJson(fakePos);
+                StartCoroutine(SendData(json));
+                #endif
+            }
+            return;
+        }
 
-                Vector3 dir = last[s] - prevLast[s];
-                cars[s].transform.rotation = Quaternion.LookRotation(dir);
+        if (received)
+        {
+            // Debug.Log("Chrono: " + chrono);
+            double diff = 0.0f;
+            for (int i = 0; i < cars.Length; i++)
+            {
+                cars[i].transform.position += moveVects[i] * Time.deltaTime;
+                diff += (new Vector3(step.cars[i].pos[0], 0.5f, step.cars[i].pos[1]) - cars[i].transform.position).magnitude;
+            }
+            diff = diff / cars.Length;
+            if (diff < 0.015)
+            {
+                received = false;
+                for (int i = 0; i < cars.Length; i++)
+                {
+                    Vector3 pos = new Vector3(step.cars[i].pos[0], 0.5f, step.cars[i].pos[1]);
+                    cars[i].transform.position = pos;
+                }
+                #if UNITY_EDITOR
+                timer = timeToUpdate; // reset the timer
+                Vector3 fakePos = new Vector3(3.44f, 0, -15.707f);
+                string json = EditorJsonUtility.ToJson(fakePos);
+                StartCoroutine(SendData(json));
+                #endif
             }
         }
     }
